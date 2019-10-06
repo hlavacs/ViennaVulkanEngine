78d544083eb16cee6a9cecbbff0000c9543e3ea5
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2019-10-06 23:20:04 +0200

    Tested function loading

---
+++ VulkanEngine/VulkanFunctions.cpp
@@ -0,0 +1,10 @@
+
+#include <vulkan\vulkan.h>
+
+#define VK_EXPORTED_FUNCTION( fun ) PFN_##fun fun;
+#define VK_GLOBAL_LEVEL_FUNCTION( fun ) PFN_##fun fun;
+#define VK_INSTANCE_LEVEL_FUNCTION( fun ) PFN_##fun fun;
+#define VK_DEVICE_LEVEL_FUNCTION( fun ) PFN_##fun fun;
+
+#include "ListOfFunctions.inl"
+
