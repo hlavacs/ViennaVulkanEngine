1b6013be8dbccb2ab536817e8379f95803541893
hlavacs <helmut.hlavacs@univie.ac.at>
2020-01-24 18:03:32 +0100

    init msvc

---
+++ ViennaVulkanEngine/ViennaVulkanEngine.cpp
@@ -0,0 +1,10 @@
+// ViennaVulkanEngine.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnViennaVulkanEngine()
+{
+}
---
+++ ViennaVulkanEngine/framework.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Selten verwendete Komponenten aus Windows-Headern ausschließen
