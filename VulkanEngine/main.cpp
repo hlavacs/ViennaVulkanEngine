97a5b7a521878c761ee6442d244a3b70bfcb35f4
ZerocksX <pavao.jerebic@gmail.com>
2020-03-12 15:45:26 +0100

    added dumping display every 5 frames

--- VulkanEngine/main.cpp
+++ VulkanEngine/main.cpp
@@ -8,8 +8,6 @@
 
 #include "VEInclude.h"
 
-
-
 namespace ve {
 
 
@@ -23,15 +21,15 @@ namespace ve {
 	//
 	class EventListenerGUI : public VEEventListener {
 	protected:
-		
+
 		virtual void onDrawOverlay(veEvent event) {
-			VESubrenderFW_Nuklear * pSubrender = (VESubrenderFW_Nuklear*)getRendererPointer()->getOverlay();
+			VESubrenderFW_Nuklear* pSubrender = (VESubrenderFW_Nuklear*)getRendererPointer()->getOverlay();
 			if (pSubrender == nullptr) return;
 
-			struct nk_context * ctx = pSubrender->getContext();
+			struct nk_context* ctx = pSubrender->getContext();
 
 			if (!g_gameLost) {
-				if (nk_begin(ctx, "", nk_rect(0, 0, 200, 170), NK_WINDOW_BORDER )) {
+				if (nk_begin(ctx, "", nk_rect(0, 0, 200, 170), NK_WINDOW_BORDER)) {
 					char outbuffer[100];
 					nk_layout_row_dynamic(ctx, 45, 1);
 					sprintf(outbuffer, "Score: %03d", g_score);
@@ -43,7 +41,7 @@ namespace ve {
 				}
 			}
 			else {
-				if (nk_begin(ctx, "", nk_rect(500, 500, 200, 170), NK_WINDOW_BORDER )) {
+				if (nk_begin(ctx, "", nk_rect(500, 500, 200, 170), NK_WINDOW_BORDER)) {
 					nk_layout_row_dynamic(ctx, 45, 1);
 					nk_label(ctx, "Game Over", NK_TEXT_LEFT);
 					if (nk_button_label(ctx, "Restart")) {
@@ -87,7 +85,7 @@ namespace ve {
 			}
 			if (g_gameLost) return;
 
-			glm::vec3 positionCube   = getSceneManagerPointer()->getSceneNode("The Cube Parent")->getPosition();
+			glm::vec3 positionCube = getSceneManagerPointer()->getSceneNode("The Cube Parent")->getPosition();
 			glm::vec3 positionCamera = getSceneManagerPointer()->getSceneNode("StandardCameraParent")->getPosition();
 
 			float distance = glm::length(positionCube - positionCamera);
@@ -99,11 +97,11 @@ namespace ve {
 					getEnginePointer()->m_irrklangEngine->play2D("media/sounds/bell.wav", false);
 				}
 
-				VESceneNode *eParent = getSceneManagerPointer()->getSceneNode("The Cube Parent");
+				VESceneNode* eParent = getSceneManagerPointer()->getSceneNode("The Cube Parent");
 				eParent->setPosition(glm::vec3(d(e), 1.0f, d(e)));
 
-				getSceneManagerPointer()->deleteSceneNodeAndChildren("The Cube"+ std::to_string(cubeid));
-				VECHECKPOINTER(getSceneManagerPointer()->loadModel("The Cube"+ std::to_string(++cubeid)  , "media/models/test/crate0", "cube.obj", 0, eParent) );
+				getSceneManagerPointer()->deleteSceneNodeAndChildren("The Cube" + std::to_string(cubeid));
+				VECHECKPOINTER(getSceneManagerPointer()->loadModel("The Cube" + std::to_string(++cubeid), "media/models/test/crate0", "cube.obj", 0, eParent));
 			}
 
 			g_time -= event.dt;
@@ -122,51 +120,91 @@ namespace ve {
 		virtual ~EventListenerCollision() {};
 	};
 
-	
+	class EventListenerFrameDump : public VEEventListenerGLFW
+	{
+	private:
+		int framesPassed = 0;
+	protected:
+		virtual void onFrameEnded(veEvent event)
+		{
+			framesPassed++;
+			if (framesPassed >= 5)
+			{
+				VkExtent2D extent = getWindowPointer()->getExtent();
+				uint32_t imageSize = extent.width * extent.height * 4;
+				VkImage image = getRendererPointer()->getSwapChainImage();
+
+				uint8_t* dataImage = new uint8_t[imageSize];
+
+				vh::vhBufCopySwapChainImageToHost(getRendererPointer()->getDevice(),
+					getRendererPointer()->getVmaAllocator(),
+					getRendererPointer()->getGraphicsQueue(),
+					getRendererPointer()->getCommandPool(),
+					image, VK_FORMAT_R8G8B8A8_UNORM,
+					VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+					dataImage, extent.width, extent.height, imageSize);
+
+				m_numScreenshot++;
+
+				std::string name("media/screenshots/screenshot" + std::to_string(m_numScreenshot - 1) + ".jpg");
+				stbi_write_jpg(name.c_str(), extent.width, extent.height, 4, dataImage, 4 * extent.width);
+				delete[] dataImage;
+				framesPassed = 0;
+			}
+
+		}
+	public:
+		///Constructor of class EventListenerCollision
+		EventListenerFrameDump(std::string name) : VEEventListenerGLFW(name) { };
+
+		///Destructor of class EventListenerCollision
+		virtual ~EventListenerFrameDump() {};
+	};
 
 	///user defined manager class, derived from VEEngine
 	class MyVulkanEngine : public VEEngine {
 	public:
 
-		MyVulkanEngine( bool debug=false) : VEEngine(debug) {};
+		MyVulkanEngine(bool debug = false) : VEEngine(debug) {};
 		~MyVulkanEngine() {};
 
 
 		///Register an event listener to interact with the user
-		
+
 		virtual void registerEventListeners() {
 			VEEngine::registerEventListeners();
 
 			registerEventListener(new EventListenerCollision("Collision"), { veEvent::VE_EVENT_FRAME_STARTED });
-			registerEventListener(new EventListenerGUI("GUI"), { veEvent::VE_EVENT_DRAW_OVERLAY});
+			registerEventListener(new EventListenerGUI("GUI"), { veEvent::VE_EVENT_DRAW_OVERLAY });
+			registerEventListener(new EventListenerFrameDump("FrameDump"), { veEvent::VE_EVENT_FRAME_ENDED });
 		};
-		
+
 
 		///Load the first level into the game engine
 		///The engine uses Y-UP, Left-handed
-		virtual void loadLevel( uint32_t numLevel=1) {
+		virtual void loadLevel(uint32_t numLevel = 1) {
 
-			VEEngine::loadLevel(numLevel );			//create standard cameras and lights
+			VEEngine::loadLevel(numLevel);			//create standard cameras and lights
 
-			VESceneNode *pScene;
-			VECHECKPOINTER( pScene = getSceneManagerPointer()->createSceneNode("Level 1", getRoot()) );
-	
+			VESceneNode* pScene;
+			VECHECKPOINTER(pScene = getSceneManagerPointer()->createSceneNode("Level 1", getRoot()));
+
 			//scene models
 
-			VESceneNode *sp1;
-			VECHECKPOINTER( sp1 = getSceneManagerPointer()->createSkybox("The Sky", "media/models/test/sky/cloudy",
-										{	"bluecloud_ft.jpg", "bluecloud_bk.jpg", "bluecloud_up.jpg", 
-											"bluecloud_dn.jpg", "bluecloud_rt.jpg", "bluecloud_lf.jpg" }, pScene)  );
+			VESceneNode* sp1;
+			VECHECKPOINTER(sp1 = getSceneManagerPointer()->createSkybox("The Sky", "media/models/test/sky/cloudy",
+				{ "bluecloud_ft.jpg", "bluecloud_bk.jpg", "bluecloud_up.jpg",
+					"bluecloud_dn.jpg", "bluecloud_rt.jpg", "bluecloud_lf.jpg" }, pScene));
 
-			VESceneNode *e4;
-			VECHECKPOINTER( e4 = getSceneManagerPointer()->loadModel("The Plane", "media/models/test", "plane_t_n_s.obj",0, pScene) );
+			VESceneNode* e4;
+			VECHECKPOINTER(e4 = getSceneManagerPointer()->loadModel("The Plane", "media/models/test", "plane_t_n_s.obj", 0, pScene));
 			e4->setTransform(glm::scale(glm::mat4(1.0f), glm::vec3(1000.0f, 1.0f, 1000.0f)));
 
-			VEEntity *pE4;
-			VECHECKPOINTER( pE4 = (VEEntity*)getSceneManagerPointer()->getSceneNode("The Plane/plane_t_n_s.obj/plane/Entity_0") );
-			pE4->setParam( glm::vec4(1000.0f, 1000.0f, 0.0f, 0.0f) );
+			VEEntity* pE4;
+			VECHECKPOINTER(pE4 = (VEEntity*)getSceneManagerPointer()->getSceneNode("The Plane/plane_t_n_s.obj/plane/Entity_0"));
+			pE4->setParam(glm::vec4(1000.0f, 1000.0f, 0.0f, 0.0f));
 
-			VESceneNode *e1,*eParent;
+			VESceneNode* e1, * eParent;
 			eParent = getSceneManagerPointer()->createSceneNode("The Cube Parent", pScene, glm::mat4(1.0));
 			VECHECKPOINTER(e1 = getSceneManagerPointer()->loadModel("The Cube0", "media/models/test/crate0", "cube.obj"));
 			eParent->multiplyTransform(glm::translate(glm::mat4(1.0f), glm::vec3(-10.0f, 1.0f, 10.0f)));
@@ -193,4 +231,3 @@ int main() {
 
 	return 0;
 }
-
