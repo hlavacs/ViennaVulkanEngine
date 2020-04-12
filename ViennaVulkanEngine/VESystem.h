640a51070550ecb95b85450e965e8f442b533060
Helmut Hlavacs <helmut.hlavacsq@univie.ac.at>
2020-04-12 08:29:32 +0200

    system class

--- ViennaVulkanEngine/VESystem.h
+++ ViennaVulkanEngine/VESystem.h
@@ -5,31 +5,44 @@ namespace vve::sysvul {
 
 	class VeSystem {
 
+	protected:
+		std::string m_systemName = "";
+		VeHandle m_systemHandle = VE_NULL_HANDLE;
+
+		VeHandle m_subscribePreupdateID = VE_NULL_HANDLE;
+		VeHandle m_subscribeUpdateID = VE_NULL_HANDLE;
+		VeHandle m_subscribePostupdateID = VE_NULL_HANDLE;
+		VeHandle m_subscribeCloseID = VE_NULL_HANDLE;
+
 	public:
-		VeSystem() {
+		VeSystem( std::string name, bool subscribe = true) {
+			m_systemName = name;
+			syseng::registerEntity(m_systemName);
+			m_systemHandle = syseng::getEntityHandle(m_systemName);
 
+			if (subscribe) {
+
+			}
 		};
 
-		~VeSystem() {
+		virtual ~VeSystem() {};
+		virtual void preupdate(VeHandle receiverID) {};
+		virtual void update(VeHandle receiverID) {};
+		virtual void postupdate(VeHandle receiverID) {};
+		virtual void close(VeHandle receiverID) {
+			if (m_subscribePreupdateID != VE_NULL_HANDLE) {
 
+			}
+			if (m_subscribeUpdateID != VE_NULL_HANDLE) {
+
+			}
+			if (m_subscribePostupdateID != VE_NULL_HANDLE) {
+
+			}
+			if (m_subscribeCloseID != VE_NULL_HANDLE) {
+
+			}
 		};
-
-		virtual void preupdate(VeHandle receiverID) {
-
-		};
-
-		virtual void update(VeHandle receiverID) {
-
-		};
-
-		void postupdate(VeHandle receiverID) {
-
-		};
-
-		void close(VeHandle receiverID) {
-
-		};
-
 	};
 
 };
