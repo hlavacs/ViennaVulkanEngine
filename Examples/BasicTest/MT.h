99a0adba6b6bb11405e2e16cb4cdc09c445a44c8
hlavacs <helmut.hlavacs@univie.ac.at>
2020-06-30 16:13:04 +0200

    VeEngine

--- Examples/BasicTest/MT.h
+++ Examples/BasicTest/MT.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#include <type_traits>
 
 #define _NODISCARD [[nodiscard]]
-//#include <experimental/coroutine>
 #include <experimental/generator>
-//#include "generator.h"
 
 
 namespace MT {
@@ -13,22 +12,16 @@ namespace MT {
     using namespace std::experimental;
 
     generator<double> fibonacci(const double ceiling) {
-        double j = 0;
-        double i = 1;
-        co_yield j;
-        if (ceiling > j) {
-            do {
-                co_yield i;
-                double tmp = i;
-                i += j;
-                j = tmp;
-            } while (i <= ceiling);
-        }
+        double i = 0;
+        while (i <= ceiling) {
+            co_yield i;
+            i++;
+        } 
     }
 
     int testFib() {
 
-        const double demo_ceiling = 10E44;
+        const double demo_ceiling = 10;
 
         auto f = fibonacci(demo_ceiling);
         auto iter = f.begin();
@@ -42,10 +35,34 @@ namespace MT {
         return 0;
     }
 
+    void g1(typename decltype(fibonacci(1))::iterator& iter) {
+        iter++;
+    }
+
+
+    int testFib2() {
+
+        const double demo_ceiling = 10;
+
+        auto f = fibonacci(demo_ceiling);
+        auto iter = f.begin();
+        auto end = f.end();
+
+        auto g = [&]() {
+            while (iter != end) {
+                const auto value = *iter;
+                std::cout << value << '\n';
+                g1( iter );
+            };
+        };
+        g();
+        return 0;
+    }
+
 
     int test() {
 
-        testFib();
+        testFib2();
         return 0;
     }
 
--- ViennaVulkanEngine/080_VEEngine.ixx
+++ ViennaVulkanEngine/080_VEEngine.ixx
@@ -12,12 +12,45 @@ import :VEMemory;
 import :VETableChunk;
 import :VETableState;
 
-
+#define _NODISCARD [[nodiscard]]
+#include <experimental/generator>
 
 export namespace vve {
 
+    using namespace std::experimental;
 
+    generator<bool> loop() {
+         
+        co_yield true;
 
+        co_yield true;
+
+        co_yield true;
+
+        co_yield false;
+
+    }
+
+    bool computeOneFrame2(typename decltype(loop())::iterator& iter) {
+        if (*iter) {
+            iter++;
+            //JREP;
+        }
+        return false;
+    }
+
+    bool go_on = true;
+    bool computeOneFrame() {
+        auto f = loop();
+        auto iter = f.begin();
+        computeOneFrame2(iter);
+        return go_on;
+    }
+
+    void runGameLoop() {
+        //JADD
+        while (computeOneFrame()) {};
+    }
 
 
 };
