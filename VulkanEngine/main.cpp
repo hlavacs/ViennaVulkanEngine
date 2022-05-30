866c870b27b9f96e6f0bb636c1a69665883c78d1
Helmut <Helmut@DESKTOP-QCHMISS>
2022-05-30 17:51:33 +0200

    structs for the physics engine

--- VulkanEngine/main.cpp
+++ VulkanEngine/main.cpp
@@ -8,7 +8,31 @@
 
 #include "VEInclude.h"
 
+//#define GLM_FORCE_PRECISION_MEDIUMP_INT
+#define GLM_FORCE_PRECISION_HIGHP_FLOAT
+#define GLM_FORCE_LEFT_HANDED
+#include "glm/glm.hpp"
 
+using real = double;
+using int_t = int64_t;
+using uint_t = uint64_t;
+
+
+template <typename T> 
+inline void hash_combine(std::size_t& seed, T const& v) {
+	seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+namespace std {
+	template <>
+	struct hash<std::pair<int_t, int_t>> {
+		std::size_t operator()(const std::pair<int_t, int_t>& p) {
+			size_t seed = std::hash<int_t>()(p.first);
+			hash_combine(seed, p.second);
+			return seed;
+		}
+	};
+}
 
 namespace ve {
 
@@ -17,7 +41,50 @@ namespace ve {
 	static std::uniform_real_distribution<> d{ -10.0f, 10.0f };		//Random numbers
 
 
+	struct Plane {
+		std::vector<uint_t> m_vertices;
+		glm::vec3 m_normal;
+	};
 
+	struct Polytope {
+		std::vector<glm::vec3> m_vertices;
+		std::vector<Plane> m_planes;
+		std::vector<std::pair<uint_t,uint_t>> m_edges;
+		glm::vec3 m_scale{ 1,1,1 };
+
+		Polytope(	std::vector<glm::vec3>&& v, std::vector<Plane>&& p,
+					std::vector<std::pair<uint_t, uint_t>>&& e, glm::vec3&& scale) 
+						: m_vertices(v), m_planes(p), m_edges(e), m_scale(scale) {};
+	};
+
+	Polytope g_cube(
+		{ {-0.5,-0.5,-0.5}, {-0.5,-0.5,0.5}, {-0.5,0.5,0.5}, {-0.5,0.5,-0.5},{0.5,-0.5,-0.5}, {0.5,-0.5,0.5}, {0.5,0.5,0.5}, {0.5,0.5,-0.5} },
+		{}, 
+		{},
+		{1,1,1});
+
+
+	struct Collider {
+		void* m_owner = nullptr;
+		Polytope& m_polytope;
+		glm::vec3 m_position{ 0, 0, 0 };
+		glm::quat m_orientation{1, 0, 0, 0};
+		glm::vec3 m_inertia{1,1,1};
+		glm::vec3 m_linear_velocity{ 0,0,0 };
+		glm::vec3 m_angular_velocity{0,0,0};
+
+		std::function<void(Collider*, void*)>* m_on_move = nullptr;
+
+		Collider(void* owner, Polytope& pol, glm::vec3 pos, glm::quat o, std::function<void(Collider*,void*)>* on_move) :
+			m_owner(owner), m_polytope(pol), m_position(pos), m_orientation(o), m_on_move(on_move) {};
+	};
+
+
+	struct Contact {
+		Collider& m_collider_A;
+		Collider& m_collider_B;
+
+	};
 
 
 	//
@@ -26,11 +93,16 @@ namespace ve {
 	class EventListenerPhysics : public VEEventListener {
 	protected:
 
-		double m_last_time = 0.0;
+		double m_last_time = 0.0;					//last time the sim was interpolated
+		double m_last_slot = 0.0;					//last time the sim was calculated
+		const double m_delta_slot = 1.0 / 60.0;		//sim frequency
+		double m_next_slot = m_delta_slot;			//next time for simulation
 
-		double m_last_slot = 0.0;
-		const double m_delta_slot = 1.0 / 60.0;
-		double m_next_slot = m_delta_slot;
+		using collider_map = std::unordered_map<void*, std::shared_ptr<Collider>>;
+		collider_map m_collider;					//main container of all colliders
+		
+		const double c_width = 5;					//grid cell width (m)
+		std::unordered_map< std::pair<int_t, int_t>, collider_map > m_grid; //broadphase grid
 
 		virtual void onFrameStarted(veEvent event) {
 			//getSceneManagerPointer()->getSceneNode("The Cube Parent")->setPosition(glm::vec3(d(e), 1.0f, d(e)));
@@ -43,14 +115,40 @@ namespace ve {
 
 			double current_time = m_last_time + event.dt;
 			while (current_time > m_next_slot) {
-				//simulate next slot 
-				//...
+				broadPhase();
+				narrowPhase();
+				update();
 				m_last_slot = m_next_slot;
 				m_next_slot += m_delta_slot;
 			}
-			//simulate current_time
-			//..
+			predict();
+			m_last_time = current_time;
 		};
+
+		const std::array<std::pair<int_t, int_t>, 6> c_pairs{ { {0,0}, {1,1}, {1,0}, {-1,-1}, {0,-1}, {1,-1} } };
+
+		void make_pairs(int_t i, int_t j) {
+
+		}
+
+		void broadPhase() {
+			for (auto& cell : m_grid) {
+				int_t i = cell.first.first;
+				int_t j = cell.first.second;
+			}
+		}
+
+		void narrowPhase() {
+
+		}
+
+		void update() {
+
+		}
+
+		void predict() {
+
+		}
 
 
 	public:
@@ -70,13 +168,15 @@ namespace ve {
 		MyVulkanEngine(veRendererType type = veRendererType::VE_RENDERER_TYPE_FORWARD, bool debug=false) : VEEngine(type, debug) {};
 		~MyVulkanEngine() {};
 
+		EventListenerPhysics *m_physics;
+
 
 		///Register an event listener to interact with the user
 		
 		virtual void registerEventListeners() {
 			VEEngine::registerEventListeners();
 
-			registerEventListener(new EventListenerPhysics("Physics"), { veEvent::VE_EVENT_FRAME_STARTED });
+			registerEventListener(m_physics = new EventListenerPhysics("Physics"), { veEvent::VE_EVENT_FRAME_STARTED });
 		};
 		
 
