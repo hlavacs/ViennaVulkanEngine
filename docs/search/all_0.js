7b7245990c65eb0532a65f3b8269cb036efe4974
hlavacs <helmut.hlavacs@univie.ac.at>
2023-12-28 16:44:27 +0100

    Doxygen

---
+++ docs/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentation_20for_20modern_20library_0',['Documentation for Modern Library',['../index.html',1,'']]]
+];
---
+++ docs/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mainpage_2emd_2',['mainpage.md',['../mainpage_8md.html',1,'']]]
+];
