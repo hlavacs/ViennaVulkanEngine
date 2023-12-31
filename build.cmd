208a27ad08ed88e89d5253e9de4b4737d902ee71
hlavacs <helmut.hlavacs@univie.ac.at>
2024-01-01 00:56:59 +0100

    build.cmd

--- build.cmd
+++ build.cmd
@@ -1,7 +1,8 @@
-cmake -S . -Bbuild
+rem cmake -S . -Bbuild -G "Visual Studio 17 2022" -T ClangCL -A x64
+cmake -S . -Bbuild -A x64
 cd build
 cmake --build . --config Release
 ctest -C Release
 cmake --build . --config Debug
-ctest -C Debug
+ctest -C Release
 cd ..
