b298722571e7c4375d7d3733b10faa2228f55f0b
hlavacs <helmut.hlavacs@univie.ac.at>
2024-10-15 10:11:23 +0200

    VECS

--- include/VEEngine.h
+++ include/VEEngine.h
@@ -12,54 +12,24 @@ namespace vve {
 	class VeEngine  {
 
 	public:
-		VeEngine(){};
-		~VeEngine(){};
-
-		void init(){
-			m_initialized = true;
-		};
-
-		void shutdown(){
-			m_running = false;
-		};
-
-		void loadLevel( const char* levelName ){
-			// Load level
-		};
-
-		void createWindow( const char* windowName, int width, int height ){
-			m_window = std::make_shared<VeWindowSDL>(windowName, width, height);
-		};
-
-		void createRenderer( const char* rendererName){
-			// Create renderer
-		};
-
-		void createCamera( const char* cameraName ){
-			// Create camera
-		};
-
-		void createSceneManager( const char* sceneManagerName ){
-			// Create scene manager
-		};
-
-		void run(){
-			if(!m_initialized){ init(); }
-			m_running = true;
-			while(m_running) {
-				// Update
-				// Render
-			}
-			shutdown();
-		};
+		VeEngine();
+		~VeEngine();
+		void init();
+		void shutdown();
+		void loadLevel( const char* levelName );
+		void createWindow( const char* windowName, int width, int height );
+		void createRenderer( const char* rendererName);
+		void createCamera( const char* cameraName );
+		void createSceneManager( const char* sceneManagerName );
+		void run();
 
 	private:
 		bool m_initialized{false};
 		bool m_running{false};
 		vecs::Registry m_registry;
-		std::unordered_map<std::string, vecs::Handle> m_nameMap;
-		std::shared_ptr<VeWindow> m_window;
-		std::shared_ptr<VeRenderer> m_renderer;
+		std::unordered_map<std::string, vecs::Handle> m_nameMap{};
+		std::shared_ptr<VeWindow> m_window{};
+		std::shared_ptr<VeRenderer> m_renderer{};
 
 	};
 };
--- src/VEEngine.cpp
+++ src/VEEngine.cpp
@@ -1,10 +1,53 @@
 #include "VEEngine.h"
 
+using namespace vve;
 
-namespace vve {
+VeEngine::VeEngine(){};
+
+VeEngine::~VeEngine(){};
+
+void VeEngine::init(){
+	m_initialized = true;
+};
+
+void VeEngine::shutdown(){
+	m_running = false;
+};
+
+void VeEngine::loadLevel( const char* levelName ){
+	// Load level
+};
+
+void VeEngine::createWindow( const char* windowName, int width, int height ){
+	m_window = std::make_shared<VeWindowSDL>(windowName, width, height);
+};
+
+void VeEngine::createRenderer( const char* rendererName){
+	// Create renderer
+};
+
+void VeEngine::createCamera( const char* cameraName ){
+	// Create camera
+};
+
+void VeEngine::createSceneManager( const char* sceneManagerName ){
+	// Create scene manager
+};
+
+void VeEngine::run(){
+	if(!m_initialized){ init(); }
+	m_running = true;
+	while(m_running) {
+		// Update
+		// Render
+	}
+	shutdown();
+};
 
 
 
-}
+
+
+
 
 
