166b83f8f0e19dca0df4ebe9bfe1f62d12fbc899
hlavacs <helmut.hlavacs@univie.ac.at>
2024-11-22 15:38:46 +0100

    REGISTRYTYPE_SEQUENTIAL

--- include/VESceneManager.h
+++ include/VESceneManager.h
@@ -32,7 +32,7 @@ namespace vve {
 		bool LoadGLTF(std::string filename);
 
     private:
-		vecs::Registry<vecs::RegistryType::SEQUENTIAL>& m_registry;
+		vecs::Registry<vecs::REGISTRYTYPE_SEQUENTIAL>& m_registry;
     };
 
 };  // namespace vve
