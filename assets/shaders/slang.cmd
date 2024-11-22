067d4ad833bef1754cf50d9da5521c8e76bb96f6
hlavacs <helmut.hlavacs@univie.ac.at>
2024-11-22 15:20:01 +0100

    slang shaders

---
+++ assets/shaders/slang.cmd
@@ -0,0 +1,6 @@
+rem slangc.exe vert.slang -target spirv -o vert.spv -entry vertexMain
+rem slangc.exe frag.slang -target spirv -o frag.spv -entry fragmentMain
+
+slangc.exe vert.slang -target spirv -o vert.spv -entry vertexMain
+slangc.exe vert.slang -target spirv -o frag.spv -entry fragmentMain
+
