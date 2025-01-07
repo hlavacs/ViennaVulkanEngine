f1576faf35e8c24ad3f4e0cb139b07cded13d285
hlavacs <helmut.hlavacs@univie.ac.at>
2025-01-07 02:03:49 +0100

    Rotation{}

--- src/VEGUI.cpp
+++ src/VEGUI.cpp
@@ -57,9 +57,7 @@ namespace vve {
             if (ImGui::Button("Load")) {                          // Buttons return true when clicked (most widgets return true when edited/activated)
 				static float x = 0.0f;
 				
-				auto handle = m_registry.Insert( Position{vec3_t(0.0f, x, 0.0f)}, 
-								   Rotation{mat3_t{1.0f}},
-								   Scale{vec3_t{1.0f, 1.0f, 1.0f}} );
+				auto handle = m_registry.Insert( Position{ { 0.0f, x, 0.0f } }, Rotation{}, Scale{});
 
 				m_engine.SendMessage( MsgLoadObject{this, nullptr, handle, {}, path_texture, path_obj} );
 				x += 2.0f;
