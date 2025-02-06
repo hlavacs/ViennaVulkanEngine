19a0ca10d6fe77aff2588aec08927a36015e18d5
hlavacs <helmut.hlavacs@univie.ac.at>
2025-02-06 11:14:36 +0100

    Added -g to shaders

--- shaders/Forward/slang.cmd
+++ shaders/Forward/slang.cmd
@@ -1,10 +1,12 @@
-slangc.exe Common.slang -target spirv -o Common.spv
+SET FLAGS=-target spirv -g
 
-slangc.exe S0100_PNUTE_vert.slang -target spirv -o S0100_PNUTE_vert.spv -entry vertexMain
-slangc.exe S0100_PNUTE_frag.slang -target spirv -o S0100_PNUTE_frag.spv -entry fragmentMain
+slangc.exe Common.slang %FLAGS% -o Common.spv
 
-slangc.exe S1000_PNC_vert.slang -target spirv -o S1000_PNC_vert.spv -entry vertexMain
-slangc.exe S1000_PNC_frag.slang -target spirv -o S1000_PNC_frag.spv -entry fragmentMain
+slangc.exe S0100_PNUTE_vert.slang %FLAGS% -o S0100_PNUTE_vert.spv -entry vertexMain
+slangc.exe S0100_PNUTE_frag.slang %FLAGS% -o S0100_PNUTE_frag.spv -entry fragmentMain
 
-slangc.exe S2000_PNO_vert.slang -target spirv -o S2000_PNO_vert.spv -entry vertexMain
-slangc.exe S2000_PNO_frag.slang -target spirv -o S2000_PNO_frag.spv -entry fragmentMain
+slangc.exe S1000_PNC_vert.slang %FLAGS% -o S1000_PNC_vert.spv -entry vertexMain
+slangc.exe S1000_PNC_frag.slang %FLAGS% -o S1000_PNC_frag.spv -entry fragmentMain
+
+slangc.exe S2000_PNO_vert.slang %FLAGS% -o S2000_PNO_vert.spv -entry vertexMain
+slangc.exe S2000_PNO_frag.slang %FLAGS% -o S2000_PNO_frag.spv -entry fragmentMain
