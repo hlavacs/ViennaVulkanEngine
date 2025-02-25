41c5547f1b290b10182200f14f975ea2004834c9
hlavacs <helmut.hlavacs@univie.ac.at>
2025-02-25 19:44:05 +0100

    cmd

--- build_Clang.cmd
+++ build_Clang.cmd
@@ -3,7 +3,7 @@ rem cd ViennaVulkanEngine
 
 rem compile with Clang
 set BUILD_TYPE=Debug
-cmake -S . -Bbuild -A x64 -G "Visual Studio 17 2022" -T ClangCL -DCMAKE_CXX_COMPILER=clang-cl++ -Bbuild -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+cmake -S . -Bbuild -A x64 -G "Visual Studio 17 2022" -T ClangCL -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl++ -Bbuild -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 cd build
 cmake --build . --config %BUILD_TYPE%
 doxygen
