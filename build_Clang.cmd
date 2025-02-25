cab72bd5274046c64205505fd56064858ef385a4
hlavacs <helmut.hlavacs@univie.ac.at>
2025-02-25 19:37:06 +0100

    clang does not work with cmake

---
+++ build_Clang.cmd
@@ -0,0 +1,10 @@
+rem git clone -b nexgen.develop https://github.com/hlavacs/ViennaVulkanEngine.git
+rem cd ViennaVulkanEngine
+
+# compile with Clang
+set BUILD_TYPE=Debug
+cmake -S . -Bbuild -A x64 -G "Visual Studio 17 2022" -T ClangCL -DCMAKE_CXX_COMPILER=clang-cl++ -Bbuild -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+cd build
+cmake --build . --config %BUILD_TYPE%
+doxygen
+cd ..
---
+++ build_MSVC.cmd
@@ -0,0 +1,10 @@
+rem git clone -b nexgen.develop https://github.com/hlavacs/ViennaVulkanEngine.git
+rem cd ViennaVulkanEngine
+
+rem compile with MSVC
+set CMAKE_BUILD_TYPE=Debug
+cmake -S . -Bbuild -A x64 -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
+cd build
+cmake --build . --config %CMAKE_BUILD_TYPE%
+doxygen
+cd ..
