a72bf09b017f63de75ccb0f63a884fa4cd8ce0c6
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2025-03-02 20:19:57 +0100

    slang

--- include/VHVulkan.h
+++ include/VHVulkan.h
@@ -72,18 +72,19 @@ namespace vh {
 	    alignas(16) glm::mat4 proj;
 	};
 
+	//param.x==1...point, param.x==2...directional, param.x==3...spotlight
 	struct LightParams {
 		alignas(16) glm::vec3 	color{1.0f, 1.0f, 1.0f}; 
 		alignas(16) glm::vec4 	param{1.0f, 1.0f, 1.0, 1.0f}; //x=type, y=intensity, z=power, w=ambient
 		alignas(16) glm::vec3 	attenuation{1.0f, 0.0f, 0.0f}; //x=constant, y=linear, z=quadratic
 	};
 
-	//param.x==1...point, param.x==2...directional, param.x==3...spotlight
+	//params.param.x==1...point, params.param.x==2...directional, params.param.x==3...spotlight
 	struct Light {
 	    alignas(16) glm::vec3 	positionW{100.0f, 100.0f, 100.0f};
 	    alignas(16) glm::vec3 	directionW{1.0f, 1.0f, 1.0f}; //always local y-axis
 	    alignas(16) LightParams params;
-		alignas(16) glm::mat3x3 lightSpaceMatrix{1.0f};
+		alignas(16) glm::mat4 lightSpaceMatrix{1.0f};
 	};
 
 	struct UniformBufferFrame {
