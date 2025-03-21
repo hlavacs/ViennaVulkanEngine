04ba3406ca4c7596d699ef491a9da3c424f84422
Mario Zugec <zugecmario@yahoo.de>
2025-03-21 14:31:34 +0100

    Add class structure

--- include/VERendererDeferred11.h
+++ include/VERendererDeferred11.h
@@ -4,5 +4,13 @@ namespace vve {
 
 	class RendererDeferred11 : public Renderer {
 
+		friend class RendererDeferred;
+
+	public:
+		RendererDeferred11(std::string systemName, Engine& engine, std::string windowName);
+		virtual ~RendererDeferred11();
+
+	private:
 	};
-}
+
+}	// namespace vve
--- src/VERendererDeferred11.cpp
+++ src/VERendererDeferred11.cpp
@@ -3,4 +3,14 @@
 
 namespace vve {
 
+	RendererDeferred11::RendererDeferred11(std::string systemName, Engine& engine, std::string windowName)
+		: Renderer(systemName, engine, windowName) {
+
+		engine.RegisterCallback({
+			{this, 3500, "INIT", [this](Message& message) { return OnInit(message); } },
+			//{this,     0, "QUIT", [this](Message& message) { return OnQuit(message); } }
+			});
+	}
+
+	RendererDeferred11::~RendererDeferred11() {};
 }
