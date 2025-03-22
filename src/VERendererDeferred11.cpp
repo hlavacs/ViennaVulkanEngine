055dacd251889ba3e2b500a5ad526a82e9a47fae
Mario Zugec <zugecmario@yahoo.de>
2025-03-22 17:34:50 +0100

    Add initial bindings

--- src/VERendererDeferred11.cpp
+++ src/VERendererDeferred11.cpp
@@ -20,10 +20,22 @@ namespace vve {
 
 		vh::RenCreateRenderPass(m_vulkanState().m_physicalDevice, m_vulkanState().m_device, m_vulkanState().m_swapChain, false, m_renderPass);
 
-		vh::RenCreateDescriptorSetLayout(m_vulkanState().m_device,
+		// TODO: maybe split uniform buffer into separate vertex and fragment buffer
+		vh::RenCreateDescriptorSetLayout(
+			m_vulkanState().m_device,
 			{
-				{	.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
-					.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT }
+				{	// Binding 0 : Vertex and Fragment uniform buffer
+					.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+					.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT },
+				{	// Binding 1 : Position
+					.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+					.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT },
+				{	// Binding 2 : Normals
+					.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+					.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT },
+				{	// Binding 3 : Albedo (Diffuse)
+					.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+					.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT },
 			},
 			m_descriptorSetLayoutPerFrame);
 
