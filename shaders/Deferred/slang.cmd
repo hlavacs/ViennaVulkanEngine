71e50163a182194f645592257c2f6e41d7813b45
Mario Zugec <zugecmario@yahoo.de>
2025-04-04 15:56:25 +0200

    Change shaders for geometry pass testing

--- shaders/Deferred/slang.cmd
+++ shaders/Deferred/slang.cmd
@@ -1,6 +1,6 @@
 SET FLAGS=-target spirv -g
 
-slangc.exe test_vert.slang %FLAGS% -o test_vert.spv -entry vertexMain
-slangc.exe test_frag.slang %FLAGS% -o test_frag.spv -entry fragmentMain
+slangc.exe test_geometry_vert.slang %FLAGS% -o test_geometry_vert.spv -entry vertexMain
+slangc.exe test_geometry_frag.slang %FLAGS% -o test_geometry_frag.spv -entry fragmentMain
 
 pause
