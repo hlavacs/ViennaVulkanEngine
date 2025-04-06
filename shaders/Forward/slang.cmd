716630915228ffe3d07ea49b58db6e12fadf45b4
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2025-04-06 09:24:02 +0200

    new shaders

--- shaders/Forward/slang.cmd
+++ shaders/Forward/slang.cmd
@@ -8,9 +8,12 @@ slangc.exe Shadow11_frag.slang %FLAGS% -o Shadow11_frag.spv -entry fragmentMain
 
 slangc.exe 0100_PNUTE_vert.slang %FLAGS% -o 0100_PNUTE_vert.spv -entry vertexMain
 slangc.exe 0100_PNUTE_frag.slang %FLAGS% -o 0100_PNUTE_frag.spv -entry fragmentMain
+slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
 
 slangc.exe 1000_PNC_vert.slang %FLAGS% -o 1000_PNC_vert.spv -entry vertexMain
 slangc.exe 1000_PNC_frag.slang %FLAGS% -o 1000_PNC_frag.spv -entry fragmentMain
+slangc.exe 1000_PNC.slang %FLAGS% -o 1000_PNC.spv
 
 slangc.exe 2000_PNO_vert.slang %FLAGS% -o 2000_PNO_vert.spv -entry vertexMain
 slangc.exe 2000_PNO_frag.slang %FLAGS% -o 2000_PNO_frag.spv -entry fragmentMain
+slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
