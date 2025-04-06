06933dabf9385ec22e62de0dc5cd4802893d5a9b
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2025-04-06 09:35:24 +0200

    unification of vert and frag shaders

--- shaders/Forward/slang.cmd
+++ shaders/Forward/slang.cmd
@@ -1,19 +1,7 @@
 SET FLAGS=-target spirv -g
 
 slangc.exe Common.slang %FLAGS% -o Common.spv
-
-slangc.exe Shadow11_vert.slang %FLAGS% -o Shadow11_vert.spv -entry vertexMain
-rem slangc.exe Shadow11_geom.slang %FLAGS% -o Shadow11_frag.spv -entry geometryMain
-slangc.exe Shadow11_frag.slang %FLAGS% -o Shadow11_frag.spv -entry fragmentMain
-
-slangc.exe 0100_PNUTE_vert.slang %FLAGS% -o 0100_PNUTE_vert.spv -entry vertexMain
-slangc.exe 0100_PNUTE_frag.slang %FLAGS% -o 0100_PNUTE_frag.spv -entry fragmentMain
+slangc.exe Shadow11.slang %FLAGS% -o Shadow11.spv
 slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
-
-slangc.exe 1000_PNC_vert.slang %FLAGS% -o 1000_PNC_vert.spv -entry vertexMain
-slangc.exe 1000_PNC_frag.slang %FLAGS% -o 1000_PNC_frag.spv -entry fragmentMain
 slangc.exe 1000_PNC.slang %FLAGS% -o 1000_PNC.spv
-
-slangc.exe 2000_PNO_vert.slang %FLAGS% -o 2000_PNO_vert.spv -entry vertexMain
-slangc.exe 2000_PNO_frag.slang %FLAGS% -o 2000_PNO_frag.spv -entry fragmentMain
 slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
