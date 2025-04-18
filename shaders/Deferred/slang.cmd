cdc870e2fdbdb533f39585f03cddb246fe4b4673
Mario Zugec <zugecmario@yahoo.de>
2025-04-19 00:41:56 +0200

    Add PNO test shaders

--- shaders/Deferred/slang.cmd
+++ shaders/Deferred/slang.cmd
@@ -1,6 +1,7 @@
 SET FLAGS=-target spirv -g
 
 slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
+slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
 slangc.exe test_lighting.slang %FLAGS% -o test_lighting.spv
 
 pause
