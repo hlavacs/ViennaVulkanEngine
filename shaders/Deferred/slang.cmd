4dbbf7bae2656e2a8b6467f0ae21b474efdc1305
Mario Zugec <zugecmario@yahoo.de>
2025-04-30 17:53:21 +0200

    Add PNC shader to def ren

--- shaders/Deferred/slang.cmd
+++ shaders/Deferred/slang.cmd
@@ -1,6 +1,7 @@
 SET FLAGS=-target spirv -g
 
 slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
+slangc.exe 1000_PNC.slang %FLAGS% -o 1000_PNC.spv
 slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
 slangc.exe test_lighting.slang %FLAGS% -o test_lighting.spv
 
