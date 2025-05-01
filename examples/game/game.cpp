f52ebf4bb0caaca82dcaaf0ecdb0fad2c3488b74
Mario Zugec <zugecmario@yahoo.de>
2025-05-01 13:07:43 +0200

    Fix cornell box colors
    - small cube is still missing

--- examples/game/game.cpp
+++ examples/game/game.cpp
@@ -46,7 +46,7 @@ class MyGame : public vve::System {
         inline static std::string plane_txt  { "assets/test/plane/grass.jpg" };
 
         inline static std::string cube_obj  { "assets/test/crate0/cube.obj" };
-        inline static std::string test_obj  { "assets/test/cornell/CornellBox-Original.obj" };
+        inline static std::string cornell_obj  { "assets/test/cornell/CornellBox-Original.obj" };
 
         bool OnLoadLevel( Message message ) {
             auto msg = message.template GetData<vve::System::MsgLoadLevel>();	
@@ -83,14 +83,16 @@ class MyGame : public vve::System {
             m_engine.SendMsg(MsgPlaySound{ vve::Filename{"assets/sounds/dance.mp3"}, -1, 50 });
 			m_engine.SendMsg(MsgSetVolume{ (int)m_volume });
 
-            // ----------------- Load Test -----------------
+            // ----------------- Load Cornell -----------------
 
-            m_handleTest = m_registry.Insert(
-                vve::Position{ { 0.0f, 0.0f, 0.0f } },
-                vve::Rotation{ mat3_t{1.0f} },
-                vve::Scale{ vec3_t{1.0f} });
-
-            m_engine.SendMsg(MsgSceneCreate{ vve::ObjectHandle(m_handleTest), vve::ParentHandle{}, vve::Filename{test_obj}, aiProcess_FlipWindingOrder });
+            m_engine.SendMsg(MsgSceneLoad{ vve::Filename{cornell_obj} });
+            m_handleCornell = m_registry.Insert(
+                vve::Position{ { 0.0f, 0.0f, -0.1f } },
+                //vve::Rotation{ mat3_t{1.0f} },
+                vve::Rotation{ mat3_t { glm::rotate(glm::mat4(1.0f), 3.14152f / 2.0f, glm::vec3(1.0f,0.0f,0.0f)) } },
+                vve::Scale{ vec3_t{1.0f} }
+            );
+            m_engine.SendMsg(MsgSceneCreate{ vve::ObjectHandle(m_handleCornell), vve::ParentHandle{}, vve::Filename{cornell_obj} });
 
             return false;
         };
@@ -160,7 +162,7 @@ class MyGame : public vve::System {
         int m_cubes_left = c_number_cubes;  
         vecs::Handle m_handlePlane{};
         vecs::Handle m_handleCube{};
-        vecs::Handle m_handleTest{};
+        vecs::Handle m_handleCornell{};
 		vecs::Handle m_cameraHandle{};
 		vecs::Handle m_cameraNodeHandle{};
 		float m_volume{MIX_MAX_VOLUME / 2.0};
