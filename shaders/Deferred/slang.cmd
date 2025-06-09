968f599ff1d3355970a34c753fd2778dbd4899a9
Mario Zugec <zugecmario@yahoo.de>
2025-06-09 12:22:45 +0200

    Change slangc compiler to v 2025.10.3-dev for testing
    - https://github.com/shader-slang/slang/releases/tag/v2025.10.3-dev
    - This is for testing purposes and should not affect backwards
    compatibility, it enforces more validations than in older versions.

--- shaders/Deferred/slang.cmd
+++ shaders/Deferred/slang.cmd
@@ -1,8 +1,19 @@
+@REM This allows passing a path argument to a different slang version
+@REM and falls back to default if no argument is passed.
+@echo off
+set SLANG_PATH=""
+if not "%1"=="" set SLANG_PATH=%1
+
 SET FLAGS=-target spirv -g
 
-slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
-slangc.exe 1000_PNC.slang %FLAGS% -o 1000_PNC.spv
-slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
-slangc.exe test_lighting.slang %FLAGS% -o test_lighting.spv
+@echo on
+%SLANG_PATH%slangc.exe -version
+
+%SLANG_PATH%slangc.exe 0100_PNUTE.slang %FLAGS% -o 0100_PNUTE.spv
+%SLANG_PATH%slangc.exe 1000_PNC.slang %FLAGS% -o 1000_PNC.spv
+%SLANG_PATH%slangc.exe 2000_PNO.slang %FLAGS% -o 2000_PNO.spv
+%SLANG_PATH%slangc.exe test_lighting.slang %FLAGS% -o test_lighting.spv
 
 pause
+
+@REM ..\..\..\..\Downloads\slang_2025-10-3-dev\bin\
