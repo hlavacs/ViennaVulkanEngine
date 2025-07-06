9aea171ee12e6e3f7d7c9b37de64679dec28be91
Mario Zugec <zugecmario@yahoo.de>
2025-07-06 23:32:37 +0200

    Add function to add multiple lights

--- examples/deferred-demo/deferred-demo.cpp
+++ examples/deferred-demo/deferred-demo.cpp
@@ -4,6 +4,7 @@
 #include <format>
 #include "VHInclude2.h"
 #include "VEInclude.h"
+#include <random>
 
 class MyGame : public vve::System {
 
@@ -95,8 +96,9 @@ class MyGame : public vve::System {
             );
             m_engine.SendMsg(MsgSceneCreate{ vve::ObjectHandle(m_handleCornell), vve::ParentHandle{}, vve::Filename{cornell_obj}, aiProcess_PreTransformVertices });
             // cornell camera position
-            m_registry.Get<vve::Position&>(m_cameraHandle)().z += 0.46f;
-            m_registry.Get<vve::Position&>(m_cameraHandle)().y -= 1.2f;
+            m_registry.Get<vve::Position&>(m_cameraNodeHandle)().x += 7.46f;
+            m_registry.Get<vve::Position&>(m_cameraNodeHandle)().y -= 4.2f;
+            m_registry.Get<vve::Position&>(m_cameraNodeHandle)().z += 1.46f;
 
             // -----------------  Fireplace -----------------
             aiPostProcessSteps flags = static_cast<aiPostProcessSteps>(aiProcess_PreTransformVertices | aiProcess_ImproveCacheLocality);
@@ -130,28 +132,28 @@ class MyGame : public vve::System {
                 sphereColor,
                 vve::MeshName{ "assets/standard/sphere.obj/sphere" }
                 );
-            m_engine.SendMsg(MsgObjectCreate{ vve::ObjectHandle(lightHandle), vve::ParentHandle{}, this });
+            m_engine.SendMsg(MsgObjectCreate{ vve::ObjectHandle(lightHandle), vve::ParentHandle{m_myParentHandle}, this });
 
 
             // -----------------  Spot Light 1 -----------------
-            vvh::Color color3{ { 0.0f, 0.0f, 0.0f, 1.0f }, { 0.1f, 0.1f, 0.9f, 1.0f }, { 0.0f, 0.0f, 0.0f, 1.0f } };
-            float intensity3 = 5.9f;
-            auto lightHandle3 = m_registry.Insert(
-                vve::Name{"SpotLight-1"},
-                vve::SpotLight{vvh::LightParams{
-        		    .color = glm::vec3(1.0f, 0.0f, 0.0f), 
-                    .params = glm::vec4(3.0f, intensity3, 10.0, 0.01f), 
-                    .attenuation = glm::vec3(1.0f, 0.09f, 0.032f),
-        	    }},
-                vve::Position{glm::vec3(20.0f, -10.0f, 10.0f)},
-                vve::Rotation{mat3_t{glm::rotate(glm::mat4(1.0f), -3.14152f / 5.0f, glm::vec3(1.0f,0.0f,0.0f)) }},
-                vve::Scale{vec3_t{0.01f, 0.05f, 0.01f}},
-                vve::LocalToParentMatrix{mat4_t{1.0f}},
-                vve::LocalToWorldMatrix{mat4_t{1.0f}},
-                color3,
-        	    vve::MeshName{"assets/standard/sphere.obj/sphere"}
-            );
-            m_engine.SendMsg(MsgObjectCreate{ vve::ObjectHandle(lightHandle3), vve::ParentHandle{}, this });
+            //vvh::Color color3{ { 0.0f, 0.0f, 0.0f, 1.0f }, { 0.1f, 0.1f, 0.9f, 1.0f }, { 0.0f, 0.0f, 0.0f, 1.0f } };
+            //float intensity3 = 5.9f;
+            //auto lightHandle3 = m_registry.Insert(
+            //    vve::Name{"SpotLight-1"},
+            //    vve::SpotLight{vvh::LightParams{
+        		  //  .color = glm::vec3(1.0f, 0.0f, 0.0f), 
+            //        .params = glm::vec4(3.0f, intensity3, 10.0, 0.01f), 
+            //        .attenuation = glm::vec3(1.0f, 0.09f, 0.032f),
+        	   // }},
+            //    vve::Position{glm::vec3(20.0f, -10.0f, 10.0f)},
+            //    vve::Rotation{mat3_t{glm::rotate(glm::mat4(1.0f), -3.14152f / 5.0f, glm::vec3(1.0f,0.0f,0.0f)) }},
+            //    vve::Scale{vec3_t{0.01f, 0.05f, 0.01f}},
+            //    vve::LocalToParentMatrix{mat4_t{1.0f}},
+            //    vve::LocalToWorldMatrix{mat4_t{1.0f}},
+            //    color3,
+        	   // vve::MeshName{"assets/standard/sphere.obj/sphere"}
+            //);
+            //m_engine.SendMsg(MsgObjectCreate{ vve::ObjectHandle(lightHandle3), vve::ParentHandle{}, this });
 
             return false;
         };
@@ -160,7 +162,7 @@ class MyGame : public vve::System {
             auto& msg = message.template GetData<vve::System::MsgUpdate>();
             m_time_left -= static_cast<float>(msg.m_dt);
             auto pos = m_registry.Get<vve::Position&>(m_cameraNodeHandle);
-            pos().z = 0.5f;
+            pos().z = 1.5f;
             if( m_state == State::STATE_RUNNING ) {
                 if( m_time_left <= 0.0f ) { 
                     m_state = State::STATE_DEAD; 
@@ -194,15 +196,19 @@ class MyGame : public vve::System {
                 | ImGuiWindowFlags_NoMove);
 
             ImGui::Text("Select Light amount:");
-            for (int i = 1; i <= 6; ++i) {
-                char buf[8];
-                sprintf(buf, "%d", i);
-                if (ImGui::Button(buf, ImVec2(30, 30))) {
-                    currentNumberLights = i;
+            static const int options[] = { 1, 5, 10, 20, 40, 80, 160 };
+            for (size_t idx = 0; idx < std::size(options); ++idx) {
+                uint16_t val = options[idx];
+                char buf[16];
+                sprintf(buf, "%d", val);
+                if (ImGui::Button(buf, ImVec2(40, 30))) {
+                    currentNumberLights = val;
+                    manageLights(val);
                 }
-                if (i < 6)
+                if (idx + 1 < std::size(options))
                     ImGui::SameLine();
             }
+
             ImGui::Text("Current: %d", currentNumberLights);
 
             ImGui::End();
@@ -210,6 +216,37 @@ class MyGame : public vve::System {
             return false;
         }
 
+        void manageLights(const uint16_t& lightCount) {
+            // deletes all lights first
+            for (auto [handle, light] : m_registry.template GetView<vecs::Handle, vve::PointLight&>()) {
+                m_engine.SendMsg(MsgObjectDestroy{ (vve::ObjectHandle)handle});
+            }
+            static constexpr vvh::Color sphereColor{ { 0.0f, 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f }, { 0.0f, 0.0f, 0.0f, 1.0f } };
+
+            for (uint16_t i = 0; i < lightCount; ++i) {
+                // Random values but still close to main scene
+                glm::vec3 pointLightPosition(randFloat(5.0f, 10.0f), randFloat(0.5f, 3.0), randFloat(1.75f, 2.5f));
+                glm::vec3 pointLightColor(randFloat(0.0f, 1.0f), randFloat(0.0f, 1.0f), randFloat(0.0f, 1.0f));
+                float intensity = randFloat(0.1f, 0.3f);
+
+                auto lightHandle = m_registry.Insert(
+                    vve::Name{ "PointLight-" + i },
+                    vve::PointLight{ vvh::LightParams{
+                        .color = pointLightColor,
+                        .params = glm::vec4(1.0f, intensity, 10.0f, 0.01f),
+                        .attenuation = glm::vec3(1.0f, 0.09f, 0.032f),
+                    } },
+                    vve::Position{ pointLightPosition },
+                    vve::Rotation{ mat3_t{1.0f} },
+                    vve::Scale{ vec3_t{0.01f, 0.01f, 0.01f} },
+                    vve::LocalToParentMatrix{ mat4_t{1.0f} },
+                    vve::LocalToWorldMatrix{ mat4_t{1.0f} },
+                    sphereColor,
+                    vve::MeshName{ "assets/standard/sphere.obj/sphere" }
+                    );
+                m_engine.SendMsg(MsgObjectCreate{ vve::ObjectHandle(lightHandle), vve::ParentHandle{}, this });
+            }
+        }
     private:
         State m_state = State::STATE_RUNNING;
         float m_time_left = c_max_time;
@@ -220,7 +257,18 @@ class MyGame : public vve::System {
 		vecs::Handle m_cameraHandle{};
 		vecs::Handle m_cameraNodeHandle{};
         vecs::Handle m_test{};
+        vecs::Handle m_myParentHandle{};
 		float m_volume{MIX_MAX_VOLUME / 2.0};
+
+        static std::mt19937& getRng() {
+            static std::mt19937 engine{ std::random_device{}() };
+            return engine;
+        }
+
+        float randFloat(float min, float max) {
+            std::uniform_real_distribution<float> dist(min, max);
+            return dist(getRng());
+        }
     };
     
     
