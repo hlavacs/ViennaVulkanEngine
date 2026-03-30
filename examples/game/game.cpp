6a3ce3199c09793623e642189d9fe60820ee75d4
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2026-03-30 02:02:55 +0200

    debugging works, dlls are in the exe folder

--- examples/game/game.cpp
+++ examples/game/game.cpp
@@ -1,6 +1,8 @@
+import std;
 import VEEngine;
 
 int main() {
     VEEngine engine;
+    std::cout << "VVE " << engine.getVersionMajor() << ".0\n";
     return engine.getVersionMajor() == 3 ? 0 : 1;
 }
